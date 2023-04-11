1b72b0c7e3bf33b966464294c1e583101ce3ebea
PaulV <40007694+paulvgithub@users.noreply.github.com>
2023-04-11 11:38:05 -0700

    Update function.js

--- script/function.js
+++ script/function.js
@@ -1,1 +1,22 @@
+  function openForm(type) {
+
+     document.getElementById("contact").style.display = "none";
+     document.getElementById("about").style.display = "none";
+
+     if (type=='contact'){
+        document.getElementById("contact").style.display = "block";
+      }
+     else if  (type=='about'){
+        document.getElementById("about").style.display = "block";
+      }
+
+
+  }
+
+  function closeForm() {
+
+     <!--hide popups-->
+     document.getElementById("contact").style.display = "none";
+     document.getElementById("about").style.display = "none";
+  }
 
