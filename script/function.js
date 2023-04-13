0334ff032bee0a16e6a2556be648fe1029fdfe6a
PaulV <40007694+paulvgithub@users.noreply.github.com>
2023-04-13 09:18:34 -0700

    Update function.js

--- script/function.js
+++ script/function.js
@@ -1,4 +1,15 @@
-  function openForm(type) {
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft= "0";
+}  
+
+
+function openForm(type) {
 
      document.getElementById("contact").style.display = "none";
      document.getElementById("about").style.display = "none";
